6ab4b560b95855c5ee71e2c9fbe04c3867acb1a5
sirenabailie <sirenabailie@gmail.com>
2024-10-05 15:25:23 -0500

    google font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,19 @@
-import { Inter } from 'next/font/google';
+'use client';
+
 import PropTypes from 'prop-types';
 import ClientProvider from '@/utils/context/ClientProvider';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '@/styles/globals.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import '@/styles/globals.css'; // Import global styles
 
-const inter = Inter({ subsets: ['latin'] });
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <head>
+        {/* Import Montserrat from Google Fonts */}
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
+      </head>
+      <body style={{ fontFamily: 'Montserrat, sans-serif' }}>
         <ClientProvider>{children}</ClientProvider>
       </body>
     </html>
@@ -18,22 +22,4 @@ export default function RootLayout({ children }) {
 
 RootLayout.propTypes = {
   children: PropTypes.node.isRequired,
-};
-
-// You can manage the metadata, tab content and info about your app dynamically using this. It will work on every page in your app:
-export const generateMetadata = async ({ params }) => {
-  // Destructure parameters or fetch necessary data here
-  const { slug } = params; // Example of accessing dynamic route params
-
-  return {
-    title: `Simply Books - ${slug || 'HOME'}`, // Dynamically set the title using route parameters
-    description: `This is a dynamically generated description for ${slug}.`, // Dynamic description
-    // Add other metadata fields as needed, like keywords, open graph tags, etc.
-    keywords: [`${slug}`, 'dynamic', 'page'],
-    openGraph: {
-      title: `Open Graph Title for ${slug}`,
-      description: `Open Graph Description for ${slug}`,
-      url: `https://yourwebsite.com/${slug}`,
-    },
-  };
 };
