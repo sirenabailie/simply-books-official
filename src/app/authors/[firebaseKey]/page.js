49641d04bb74d5c0c9d4abfa8f241c3f246ffdf6
sirenabailie <sirenabailie@gmail.com>
2024-10-04 21:38:54 -0500

    ViewAuthors functioning TODO: edit

--- src/app/authors/[firebaseKey]/page.js
+++ src/app/authors/[firebaseKey]/page.js
@@ -1,5 +1,46 @@
-import React from 'react';
+/* eslint-disable @next/next/no-img-element */
 
-export default function ViewAuthor() {
-  return <div>Test</div>;
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { viewAuthorDetails } from '../../../api/mergedData';
+
+export default function ViewAuthor({ params }) {
+  const [authorDetails, setAuthorDetails] = useState({});
+
+  // grab firebaseKey from url
+  const { firebaseKey } = params;
+
+  // make call to API layer to get the data
+  useEffect(() => {
+    viewAuthorDetails(firebaseKey).then(setAuthorDetails);
+  }, [firebaseKey]);
+
+  return (
+    <div className="mt-5 d-flex flex-wrap">
+      <div className="text-white ms-5 details">
+        <h5>
+          {authorDetails.first_name} {authorDetails.last_name}
+          {authorDetails.favorite ? ' 🤍' : ''}
+        </h5>
+        Email: {authorDetails.email}
+        <h6>Books:</h6>
+        {authorDetails.books && authorDetails.books.length > 0 ? (
+          <ul>
+            {authorDetails.books.map((book) => (
+              <li key={book.firebaseKey}>{book.title}</li>
+            ))}
+          </ul>
+        ) : (
+          <p>No books available for this author.</p>
+        )}
+        <hr />
+      </div>
+    </div>
+  );
 }
+
+ViewAuthor.propTypes = {
+  params: PropTypes.objectOf({}).isRequired,
+};
